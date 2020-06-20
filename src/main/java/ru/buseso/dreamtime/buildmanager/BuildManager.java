0255a37d79199202a902efa3d80f7ab89c80d62c
iBuseWinner <44753872+iBuseWinner@users.noreply.github.com>
2020-06-21 00:20:03 +0300

    создание проджекта бебебе

---
+++ src/main/java/ru/buseso/dreamtime/buildmanager/BuildManager.java
@@ -0,0 +1,17 @@
+package ru.buseso.dreamtime.buildmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BuildManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
