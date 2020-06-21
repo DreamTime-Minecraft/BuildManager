90b30b61f1a12de7e9cc46700257489196769029
iBuseWinner <44753872+iBuseWinner@users.noreply.github.com>
2020-06-21 13:39:36 +0300

    Продолжение коамы

--- src/main/java/ru/buseso/dreamtime/buildmanager/Events/BMCommand.java
+++ src/main/java/ru/buseso/dreamtime/buildmanager/Events/BMCommand.java
@@ -1,12 +1,15 @@
 package ru.buseso.dreamtime.buildmanager.Events;
 
 import com.google.common.base.Strings;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import ru.buseso.dreamtime.buildmanager.BuildManager;
 import ru.buseso.dreamtime.buildmanager.utils.BMWorld;
+import ru.buseso.dreamtime.buildmanager.utils.Games;
+import ru.buseso.dreamtime.buildmanager.utils.Progress;
 import ru.buseso.dreamtime.buildmanager.utils.Utils;
 
 import java.util.Arrays;
@@ -43,6 +46,107 @@ public class BMCommand implements CommandExecutor {
             } else {
                 help = true;
             }
+        } else if(args.length == 2) { help = true; }
+        else if(args.length == 3) {
+            if(args[0].equalsIgnoreCase("build")) {
+                String world = p.getWorld().getName();
+
+                BMWorld bmw = null;
+                for(BMWorld w : BuildManager.worlds) {
+                    if(w.getId().equals(world)) {
+                        bmw = w;
+                        break;
+                    }
+                }
+
+                if(bmw == null) {
+                    p.sendMessage(Utils.fixColor(BuildManager.prefix+"&4Мир не занесён в конфиг менеджера! " +
+                            "Сообщите об этой ошибке &cАдминистрации &9Dream&bTime&4'а!"));
+                    return false;
+                }
+
+                if(!p.getName().equals(bmw.getOwner())) {
+                    p.sendMessage(Utils.fixColor(BuildManager.prefix+"&4Вы не являетесь владельцем данного мира!"));
+                    return false;
+                }
+
+                if(args[1].equalsIgnoreCase("name")) {
+                    String name = args[2];
+                    bmw.setName(name);
+                    p.sendMessage(Utils.fixColor(BuildManager.prefix+"&aНазвание мира изменено на &b"+name+"&a!"));
+                } else if(args[1].equalsIgnoreCase("desc")) {
+                    String desc = args[2].replace('_', ' ');
+                    bmw.setDescription(desc);
+                    p.sendMessage(Utils.fixColor(BuildManager.prefix+"&aОписание мира изменено на &b"+desc+"&a!"));
+                } else if(args[1].equalsIgnoreCase("private")) {
+                    String privat = args[2];
+                    boolean priv = Boolean.parseBoolean(privat);
+                    bmw.setPrivat(priv);
+                    p.sendMessage(Utils.fixColor(BuildManager.prefix+"&aТеперь мир " +
+                            "" + (priv ? "&2открыт" : "&cзакрыт") + "&a для общего посещения"));
+                } else if(args[1].equalsIgnoreCase("game")) {
+                    String game = args[2].toUpperCase();
+                    Games games = null;
+                    try {
+                        games = Games.valueOf(game);
+                    }catch (IllegalArgumentException e) {
+                        p.sendMessage(Utils.fixColor(BuildManager.prefix+"&4Доступные цели постройки: "+Games.values()));
+                        return false;
+                    }
+
+                    bmw.setGame(games);
+                    p.sendMessage(Utils.fixColor(BuildManager.prefix+"&aЦель постройки изменена на "+game));
+                } else if(args[1].equalsIgnoreCase("progress")) {
+                    String progress = args[2].toUpperCase();
+                    Progress prog = null;
+                    try {
+                        prog = Progress.valueOf(progress);
+                    }catch (IllegalArgumentException e) {
+                        p.sendMessage(Utils.fixColor(BuildManager.prefix+"&4Доступные прогрессы постройки: "+Progress.values()));
+                        return false;
+                    }
+
+                    bmw.setProgress(prog);
+                    p.sendMessage(Utils.fixColor(BuildManager.prefix+"&aПрогресс постройки изменен на "+progress));
+                } else {
+                    help = true;
+                }
+            } else {
+                help = true;
+            }
+        }
+        else if(args.length == 4) {
+            if(args[0].equalsIgnoreCase("build")) {
+                if(args[1].equalsIgnoreCase("builder")) {
+                    String world = p.getWorld().getName();
+
+                    BMWorld bmw = null;
+                    for(BMWorld w : BuildManager.worlds) {
+                        if(w.getId().equals(world)) {
+                            bmw = w;
+                            break;
+                        }
+                    }
+
+                    if(bmw == null) {
+                        p.sendMessage(Utils.fixColor(BuildManager.prefix+"&4Мир не занесён в конфиг менеджера! " +
+                                "Сообщите об этой ошибке &cАдминистрации &9Dream&bTime&4'а!"));
+                        return false;
+                    }
+
+                    if(args[2].equalsIgnoreCase("add")) {
+
+                    } else if(args[2].equalsIgnoreCase("remove")) {
+
+                    } else {
+                        help = true;
+                    }
+                } else {
+                    help = true;
+                }
+            } else {
+                help = true;
+            }
         }
 
         if(help) {
@@ -59,9 +163,10 @@ public class BMCommand implements CommandExecutor {
         p.sendMessage(Utils.fixColor("&a> &7/bm build builder remove <ник> - убрать игрока из строителей в мире (String)"));
         p.sendMessage(Utils.fixColor("&a> &7/bm build progress <прогресс> - установить прогресс в мире (Progress)"));
         p.sendMessage(Utils.fixColor("&a> &7/bm build name <название> - установить название для мира (String)"));
-        p.sendMessage(Utils.fixColor("&a> &7/bm build desc <описание> - установить описание для мира (String)"));
+        p.sendMessage(Utils.fixColor("&a> &7/bm build desc <описание> - установить описание для мира. _ ставит пробел (String)"));
         p.sendMessage(Utils.fixColor("&a> &7/bm build private <приватность> - установить приватность для мира (Boolean)"));
         p.sendMessage(Utils.fixColor("&a> &7/bm build game <игра> - установить для чего строится постройка (Games)"));
+        p.sendMessage(Utils.fixColor("&a> &7/bm build progress <стадия> - установить прогресс для мира (Progress)"));
     }
 
     private void sendInfo(Player p, BMWorld world) {
